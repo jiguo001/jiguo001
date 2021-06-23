7cc72c83cb75650b87cf14f7e9dba935cba8b978
921377220 <921377220@qq.com>
2021-06-23 15:19:07 +0800

    导购完成

--- guid/zuire.js
+++ guid/zuire.js
@@ -1,9 +1,10 @@
-$(function() {
+$(function () {
+
     $.ajax({
-        url: "http://192.168.31.250:3000/guid/hot",
+        url: "http://localhost:3000/guid/hot",
         type: "GET",
         dateType: "JSON",
-        success: function(date) {
+        success: function (date) {
             console.log(date);
             // console.log(date[0]);
             // // console.log(date[0][0]);
@@ -16,12 +17,13 @@ $(function() {
             // date.each(function(index) {
             //     console.log(index);
             // })
-            $.each(date, function(i, k) {
+            $.each(date, function (i, k) {
                 var str = k.img;
                 var txt = k.text;
                 // console.log(str);
                 // console.log(txt);
-                var isrc = str.replace("192.168.1.64", "192.168.31.250");
+                var isrc = str.replace("192.168.1.64", "localhost");
+                console.log(isrc);
                 // console.log(isrc);
                 var ndv = $("<div></div>")
                 var nimg = $("<img>").attr("src", isrc);
